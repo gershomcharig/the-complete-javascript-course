d6d903b610f4dd87552ec74564e73854a95412d8
gershomcharig <gershom.charig@gmail.com>
2022-04-29 17:38:23 +0100

    More progress and coding challenge

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -61,6 +61,36 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+const displayMovement = function (movements) {
+  // Create a function to display the movements
+
+  containerMovements.innerHTML = "";
+  // Manipulates the HTML code to empty the initial content of the div.
+
+  movements.forEach(function (mov, i) {
+    // For each element of the "movements" array
+
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    // A variable to determine whether the movement is a deposit or a withdrawal. This is needed in the next block.
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>
+    `;
+    // The HTML code that we need to add to the DOM for each element of the array, saved into a variable
+
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+    // This adds the HTML specified before, immediately after the beginning of the ".movements" block.
+  });
+};
+
+displayMovement(account1.movements);
+// Calls the above function for account1
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -161,4 +191,36 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function (element, index, array) {
   console.log(`${index}: ${element}`);
 });
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+
+const checkDogs = function (juliaDogs, kateDogs) {
+  const dogs = juliaDogs.slice(1, -2).concat(kateDogs);
+  dogs.forEach(function (age, i) {
+    age < 3
+      ? console.log(`Dog number ${i + 1} is still a puppy 🐶`)
+      : console.log(`Dog number ${i + 1} is an adult and is ${age} years old.`);
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 */
