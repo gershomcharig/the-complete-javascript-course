744b33d30f5c51cc93a7a9275d98b9df057c12cf
gershomcharig <gershom.charig@gmail.com>
2022-05-06 09:42:10 +0100

    Coding challenge on map, filter and reduce

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -87,9 +87,17 @@ const displayMovement = function (movements) {
     // This adds the HTML specified before, immediately after the beginning of the ".movements" block.
   });
 };
-
 displayMovement(account1.movements);
 // Calls the above function for account1
+
+const calcPrintBalance = function (movements) {
+  const balance = movements.reduce(
+    (accumulator, movement) => accumulator + movement,
+    0
+  );
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcPrintBalance(account1.movements);
 
 // const user = "Steven Thomas Williams"; // We want to call this user "stw"
 // const username = user
@@ -114,8 +122,7 @@ const createUsernames = function (accounts) {
   // For each account in the "accounts" array we take the "owner" property, turn it into a username which is the first letters of the name in lowercase, then store it back into the object as a new "username" property.
 };
 
-console.log(createUsernames(accounts));
-console.log(accounts);
+createUsernames(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -266,6 +273,7 @@ const movementsDescriptions = movements.map(
 );
 */
 
+/*
 const deposits = movements.filter((mov) => mov > 0);
 const withdrawals = movements.filter((mov) => mov < 0);
 // Accumulator -> snowball effect
@@ -274,3 +282,48 @@ const balance = movements.reduce(function (accumulator, element, index, array) {
 }, 0);
 
 console.log(deposits, withdrawals, balance);
+
+// Maximum value of movements
+
+const max = movements.reduce((accumulator, movement) => {
+  if (accumulator > movement) {
+    return accumulator;
+  } else {
+    return movement;
+  }
+}, movements[0]);
+
+console.log(max);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+
+
+const calcAverageHumanAge = function (dogAges) {
+  const humanAges = dogAges
+    .map((dogAge) => (dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4))
+    .filter((humanAge) => humanAge >= 18);
+  return humanAges.reduce((a, b) => a + b, 0) / humanAges.length;
+};
+
+console.log(
+  calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]),
+  calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
+);
+*/
